ad69c2560a1cf568380b4800a7cad9d4bd2addb8
kittchaizayo@outlook.co.th <kittchaizayo@outlook.co.th>
2019-02-27 14:52:56 +0700

    add Page

--- app/src/main/java/kittichai/th/bsruservice/MainFragment.java
+++ app/src/main/java/kittichai/th/bsruservice/MainFragment.java
@@ -2,10 +2,12 @@ package kittichai.th.bsruservice;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -14,10 +16,66 @@ import android.view.ViewGroup;
 public class MainFragment extends Fragment {
 
 
-    public MainFragment() {
-        // Required empty public constructor
-    }
+    public MainFragment() {}
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Register Controller
+
+        registerController();
+        OnePage();
+        TwoPage();
+
+    } //Main Method
+    private void OnePage() {
+        TextView textView = getView().findViewById(R.id.txtOnePage);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.layoutMainFragmant,new OnePage())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+    private void TwoPage() {
+        TextView textView = getView().findViewById(R.id.txtTwoPage);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.layoutMainFragmant,new TwoPage())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+    private void registerController() {
+    TextView textView = getView().findViewById(R.id.txtRegister);
+    textView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            //Replace Fragment
+            getActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.layoutMainFragmant,new RegisterFragment())
+                    .addToBackStack(null)
+                    .commit();
+        }
+    });
+}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,5 +83,6 @@ public class MainFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
+
 
 }
